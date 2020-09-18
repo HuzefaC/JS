090d31c6c68b69485848c9b774eae5088c565802
HuzefaC <huzefachabukswar@gmail.com>
2020-09-18 18:05:09 +0530

    Added index and app

---
+++ app.js
@@ -0,0 +1,18 @@
+// var, let, const
+
+var name1 = 'Zoro';
+
+let name2 = 'Luffy'
+
+const name = 'One Piece' // value cannot be changed and has to be initialized
+
+console.log(name1, name2, name);
+
+// init 
+
+var temp;
+console.log(temp);
+
+// variable naming convention
+// can include letters, numbers, $, _
+// variable name cannot beign with a number
