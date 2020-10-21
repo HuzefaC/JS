faa4788144d21ee439eb8f563b8f3d647caf31cd
HuzefaC <huzefachabukswar@gmail.com>
2020-10-21 22:00:27 +0530

    Done

--- DOMManipulationAndEvents/DOMSelectors/app.js
+++ DOMManipulationAndEvents/DOMSelectors/app.js
@@ -22,3 +22,25 @@ document.querySelector('li').style.color = 'red';
 document.querySelector('li:last-child').style.color = 'blue';
 document.querySelector('li:nth-child(3)').style.color = 'cyan';
 document.querySelector('li:nth-child(4)').style.color = 'magenta';
+
+// Multiple element selectors
+
+const items = document.getElementsByClassName('collection-item');
+console.log(items);
+console.log(items[0]);
+
+const liList = document.querySelectorAll('li');
+console.log(liList);
+
+const lis = document.getElementsByTagName('li');
+console.log(lis);
+
+const oddLi = document.querySelectorAll('li:nth-child(odd)');
+const evenLi = document.querySelectorAll('li:nth-child(even)');
+
+oddLi.forEach((li) => {
+  li.style.background = '#ccc';
+});
+evenLi.forEach((li) => {
+  li.style.background = '#f4f4f4';
+});
