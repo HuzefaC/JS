b309b517756586d4395dd10ad70c90196a5cedd0
HuzefaC <huzefachabukswar@gmail.com>
2020-11-02 17:02:38 +0530

    Done

---
+++ AsynchronousJavaScript/AjaxAndXhr/app.js
@@ -0,0 +1,9 @@
+// What is Ajax?
+// - Ajax stands for Asynchronous JavaScript And XML
+// - It is a Set of web technologies
+// - Send and Recieve data asynchronously
+// - Does not interfer with the current page
+// - JSON has replaced XML for the most part
+
+// What is XHR?
+// - XHR stands for XmlHttpRequest
