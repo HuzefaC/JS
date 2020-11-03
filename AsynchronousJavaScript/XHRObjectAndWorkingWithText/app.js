56e8f57a3ee78075ddeaa507b537991febd55e4c
HuzefaC <huzefachabukswar@gmail.com>
2020-11-03 20:37:46 +0530

    Done

--- AsynchronousJavaScript/XHRObjectAndWorkingWithText/app.js
+++ AsynchronousJavaScript/XHRObjectAndWorkingWithText/app.js
@@ -3,6 +3,12 @@
 // 403 - Forbidden
 // 404 - Not found
 
+// 0 	UNSENT 	Client has been created. open() not called yet.
+// 1 	OPENED 	open() has been called.
+// 2 	HEADERS_RECEIVED 	send() has been called, and headers and status are available.
+// 3 	LOADING 	Downloading; responseText holds partial data.
+// 4 	DONE 	The operation is complete.
+
 document.getElementById('button').addEventListener('click', loadData);
 
 function loadData() {
@@ -16,8 +22,29 @@ function loadData() {
   // Action to take when the loading process is done
   xhr.onload = function () {
     if (xhr.status === 200) {
-      console.log(xhr.responseText);
+      //console.log(xhr.responseText);
+      document.getElementById(
+        'output'
+      ).innerHTML = `<h1>${xhr.responseText}</h1>`;
     }
+  };
+
+  // Older Syntax
+  // xhr.onreadystatechange = function () {
+  //   console.log('READYSTATE', xhr.readyState);
+  //   if (xhr.status === 200 && xhr.readyState === 4) {
+  //     console.log(xhr.responseText);
+  //   }
+  // };
+
+  // Optional - Used for Loaders
+  xhr.onprogress = function () {
+    console.log('READYSTATE', xhr.readyState);
+  };
+
+  // Runs if there are any errors
+  xhr.onerror = function () {
+    console.log('Fetching error...');
   };
   xhr.send();
 }
