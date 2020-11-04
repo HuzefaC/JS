3113639066e89450b8eb3c7644ebd663a0c94008
HuzefaC <huzefachabukswar@gmail.com>
2020-11-04 20:54:32 +0530

    Done

--- AsynchronousJavaScript/CallbackFunctions/app.js
+++ AsynchronousJavaScript/CallbackFunctions/app.js
@@ -1,43 +1,25 @@
-// What is an API?
-/*
-  - API stands for Application Programming Interface
-  - Contract provided by one piece of software ot another
-  - Structured request and response
-  - We just worked with an API that takes a request and responds with jokes
-*/
+// A callback function is a function that is passed as parameter to another function and than it runs inside that function body
 
-// What is an REST API?
-/*
-  - REST stands for Representational State Transfer.
-  - Architecture style for designing networked applications.
-  - Relies on stateless, client-server protocol, almost always HTTP.
-  - Treats server objects as resources that can be created and destroyed 
-  - Can be virtually used by any programming language
-  - All APIS have their own rules and structure
-*/
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' },
+];
 
-// HTTP Requests
-/*
-1   GET
-The GET method is used to retrieve information from the given server using a given URI. Requests using GET should only retrieve data and should have no other effect on the data.
+function createPost(post, callback) {
+  setTimeout(function () {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
 
-2 	HEAD
-Same as GET, but it transfers the status line and the header section only.
+function getPosts() {
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
 
-3 	POST
-A POST request is used to send data to the server, for example, customer information, file upload, etc. using HTML forms.
-
-4 	PUT
-Replaces all the current representations of the target resource with the uploaded content.
-
-5 	DELETE
-Removes all the current representations of the target resource given by URI.
-
-6 	CONNECT
-Establishes a tunnel to the server identified by a given URI.
-
-7 	OPTIONS
-Describe the communication options for the target resource.
-
-8 	TRACE
-Performs a message loop back test along with the path to the target resource. */
+createPost({ title: 'Post Three', body: 'This is post Three' }, getPosts);
