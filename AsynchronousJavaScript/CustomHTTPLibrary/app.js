439def5763a1a17afdb8cd505b7452016685df1c
HuzefaC <huzefachabukswar@gmail.com>
2020-11-05 16:55:03 +0530

    Done

--- AsynchronousJavaScript/CustomHTTPLibrary/app.js
+++ AsynchronousJavaScript/CustomHTTPLibrary/app.js
@@ -1,43 +1,56 @@
-// What is an API?
-/*
-  - API stands for Application Programming Interface
-  - Contract provided by one piece of software ot another
-  - Structured request and response
-  - We just worked with an API that takes a request and responds with jokes
-*/
+const http = new easyHttp();
 
-// What is an REST API?
-/*
-  - REST stands for Representational State Transfer.
-  - Architecture style for designing networked applications.
-  - Relies on stateless, client-server protocol, almost always HTTP.
-  - Treats server objects as resources that can be created and destroyed 
-  - Can be virtually used by any programming language
-  - All APIS have their own rules and structure
-*/
+// GET POSTS
+// http.get('https://jsonplaceholder.typicode.com/posts', function (
+//   err,
+//   response
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
 
-// HTTP Requests
-/*
-1   GET
-The GET method is used to retrieve information from the given server using a given URI. Requests using GET should only retrieve data and should have no other effect on the data.
+// Create the Data
 
-2 	HEAD
-Same as GET, but it transfers the status line and the header section only.
+const data = {
+  title: 'Custom Post title',
+  body: 'Custom Post body',
+};
 
-3 	POST
-A POST request is used to send data to the server, for example, customer information, file upload, etc. using HTML forms.
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
 
-4 	PUT
-Replaces all the current representations of the target resource with the uploaded content.
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
 
-5 	DELETE
-Removes all the current representations of the target resource given by URI.
-
-6 	CONNECT
-Establishes a tunnel to the server identified by a given URI.
-
-7 	OPTIONS
-Describe the communication options for the target resource.
-
-8 	TRACE
-Performs a message loop back test along with the path to the target resource. */
+// Delete posts
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (
+  err,
+  response
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
