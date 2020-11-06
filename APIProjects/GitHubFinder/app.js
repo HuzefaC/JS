76efd70679601aabacd5f2eb9fc8408009e6937c
HuzefaC <huzefachabukswar@gmail.com>
2020-11-06 17:59:21 +0530

    Partial

--- APIProjects/GitHubFinder/app.js
+++ APIProjects/GitHubFinder/app.js
@@ -1,11 +1,28 @@
+// Init Github
+const gitHub = new GitHub();
+
+// Init UI
+const ui = new UI();
 // Search Input
 const searchUser = document.getElementById('searchUser');
-
 // Search Input event listener
 searchUser.addEventListener('keyup', (e) => {
   // Get User search query
   const userText = e.target.value;
+
   if (userText !== '' || userText !== null) {
-    console.log(userText);
+    // Make a http call
+    gitHub.getUser(userText).then((data) => {
+      if (data.profileData.message === 'Not Found') {
+        // Show alert
+        //ui.alert(data.profileData);
+        console.log(data);
+      } else {
+        // Show Profile
+        ui.showProfile(data.profileData);
+      }
+    });
+  } else {
+    // Clear Profile
   }
 });
--- APIProjects/GitHubFinder/github.js
+++ APIProjects/GitHubFinder/github.js
@@ -6,15 +6,16 @@ class GitHub {
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      // `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}`
     );
-    const repoResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`
-    );
+    // const repoResponse = await fetch(
+    //   `https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    // );
 
     const profileData = await profileResponse.json();
-    const repoData = await repoResponse.json();
+    //const repoData = await repoResponse.json();
 
-    return { profileData, repoData };
+    return { profileData };
   }
 }
