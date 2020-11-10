6c2da3600cfd4132a11578da5e15e985e4c78b50
HuzefaC <huzefachabukswar@gmail.com>
2020-11-10 20:37:05 +0530

    Added files

--- ErrorHandlingAndRegularExpression/RegularExpressionPart2/app.js
+++ ErrorHandlingAndRegularExpression/RegularExpressionPart2/app.js
@@ -30,7 +30,21 @@ regex = /Hel{2, }o/i; // Letter before must occur atleast {m} times
 // Paranthesis - Used for Grouping
 regex = /([0-9]x){3}/i; // Number between 0-9 followed by x three times
 
-const str = '3x3x3x';
+// Shorthand character classes
+regex = /\w/; // Word character - alphanumeric or _
+regex = /\w+/; // + means one or more
+regex = /\W/; // Non-Word character i.e anything other than alphanumeric or _
+regex = /\d+/; // Match any digits
+regex = /\D/; // Non Digit
+regex = /\s/; // Match the whitespace characters
+regex = /\S/; // Match the Non-whitespace characters
+regex = /\S/; // Match the Non-whitespace characters
+regex = /Hell\b/i; // Word boundry
+
+// Assertions
+regex = /x(?=y)/; // Match x only if followed by y
+
+const str = 'Hello, welcome to hell';
 //const result = ;
 
 function regexTest(regex, str) {
