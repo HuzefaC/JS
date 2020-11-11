20fc459796370678a942f1ad922f2cd39c303dca
HuzefaC <huzefachabukswar@gmail.com>
2020-11-11 17:08:32 +0530

    Done

---
+++ OtherNewFeatures/IteratorsAndGenerators/app.js
@@ -0,0 +1,71 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg',
+  },
+  {
+    name: 'Jen Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/api/portraits/women/82.jpg',
+  },
+  {
+    name: 'John Cena',
+    age: 38,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston TX',
+    image: 'https://randomuser.me/api/portraits/men/80.jpg',
+  },
+];
+
+const profiles = profileIterator(data);
+
+// Call first Profile
+nextProfile();
+
+// Next event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next profile display function
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  if (currentProfile !== undefined) {
+    document.getElementById('profileDisplay').innerHTML = `
+    <ul class="list-group">
+      <li class="list-group-item">Name: ${currentProfile.name}</li>
+      <li class="list-group-item">Age: ${currentProfile.age}</li>
+      <li class="list-group-item">Location: ${currentProfile.location}</li>
+      <li class="list-group-item">Gender:  ${currentProfile.gender}</li>
+      <li class="list-group-item">Preference: ${currentProfile.lookingfor}</li>
+    </ul>
+  `;
+
+    document.getElementById('imageDisplay').innerHTML = `
+    <img src="${currentProfile.image}">
+  `;
+  } else {
+    // No more profiles
+    window.location.reload();
+  }
+}
+
+// Profile iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function () {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    },
+  };
+}
